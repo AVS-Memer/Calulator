00c600c5f6ae01e515f932c5eec9985490aea5bc
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-02-24 11:12:41 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,8 @@
 let num1 = prompt("What is the first number?");
 let op = prompt("What is the operator?");
 let num2 = prompt("What is the second number?");
-if (Number.isFinite(num1) && Number.isFinite(num2)) {
-  if (op === "+") {alert(num1 + " + " + num2 + " = " + (+num1 + +num2));}
-  else if (op === "-") {alert(num1 + " - " + num2 + " = " + (+num1 - +num2));}
-  else if (op === "*") {alert(num1 + " * " + num2 + " = " + (+num1 * +num2));}
-  else if (op === "/") {alert(num1 + " / " + num2 + " = " + (+num1 / +num2));}
-  else {alert("operation error");}
-} else {alert("numerical error");}
+if (op === "+") {alert(num1 + " + " + num2 + " = " + (+num1 + +num2));}
+else if (op === "-") {alert(num1 + " - " + num2 + " = " + (+num1 - +num2));}
+else if (op === "*") {alert(num1 + " * " + num2 + " = " + (+num1 * +num2));}
+else if (op === "/") {alert(num1 + " / " + num2 + " = " + (+num1 / +num2));}
+else {alert("operation error");}
