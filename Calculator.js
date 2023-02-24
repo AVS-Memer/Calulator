daf9d99685fc280a6772bb31195f0c1bc13435f8
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-02-24 11:06:25 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,7 @@
 let num1 = prompt("What is the first number?");
 let op = prompt("What is the operator?");
 let num2 = prompt("What is the second number?");
-if (Number.isFinite(num1) and Number.isFinite(num2)) {
+if (Number.isFinite(num1) && Number.isFinite(num2)) {
 if (op === "+") {alert(num1 + " + " + num2 + " = " + (+num1 + +num2));}
 else if (op === "-") {alert(num1 + " - " + num2 + " = " + (+num1 - +num2));}
 else if (op === "*") {alert(num1 + " * " + num2 + " = " + (+num1 * +num2));}
