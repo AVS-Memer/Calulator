15543ab25f0cd86cbb04be9ecc08d4349818cd5b
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-02-24 11:31:56 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -5,4 +5,6 @@ if (op === "+") {alert(num1 + " + " + num2 + " = " + (+num1 + +num2));}
 else if (op === "-") {alert(num1 + " - " + num2 + " = " + (+num1 - +num2));}
 else if (op === "*") {alert(num1 + " * " + num2 + " = " + (+num1 * +num2));}
 else if (op === "/") {alert(num1 + " / " + num2 + " = " + (+num1 / +num2));}
+else if (op === "%") {alert(num1 + "% of " + num2 " = " + (+num1 * +num2 / 100));}
+else if (op === "R") {alert(num1 + "/" + num2 + " = " Math.floor(+num1 / +num2) + "R" + (+num1 % +num2));}
 else {alert("operation error");}
