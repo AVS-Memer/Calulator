76e50ab01f949851c57c1daecf3bb1665f7e3891
AVS <123315354+AVS-Memer@users.noreply.github.com>
2023-03-02 18:19:54 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,24 @@
+let numofnums = prompt("How many numbers?");
 let num1 = prompt("What is the first number?");
 let op = prompt("What is the operator?");
+if (numofnums == 1) {
+  if (op == "++") {alert(num1 + "++ = " + num1+1);}
+  else if (op == "--") {alert(num1 + "-- = " + num1-1);}
+  else if (op == "%") {alert(num1 + "% = " + num1/100);}
+  else if (op == "*2") {alert(num1 + "*2 = " num1*2);}
+  else if (op == "*3") {alert(num1 + "*3 = " num1*3);}
+  else if (op == "/2") {alert(num1 + "/2 = " num1/2);}
+  else if (op == "^2") {alert(num1 + "^2 = " Math.pow(num1,2));}
+  else if (op == "^3") {alert(num1 + "^3 = " Math.pow(num1,3));}
+  } else {alert("operation error");}
+} else {
 let num2 = prompt("What is the second number?");
-if (op === "+") {alert(num1 + " + " + num2 + " = " + (+num1 + +num2));}
-else if (op === "-") {alert(num1 + " - " + num2 + " = " + (+num1 - +num2));}
-else if (op === "*") {alert(num1 + " * " + num2 + " = " + (+num1 * +num2));}
-else if (op === "/") {alert(num1 + " / " + num2 + " = " + (+num1 / +num2));}
-else if (op === "%") {alert(num1 + "% of " + num2 " = " + (+num1 * +num2 / 100));}
-else if (op === "^") {alert(num1 + "^" + num2 " = " + Math.pow(+num1 * +num2);}
-else if (op === "R") {alert(num1 + "/" + num2 + " = " Math.floor(+num1 / +num2) + "R" + (+num1 % +num2));}
+if (op == "+") {alert(num1 + " + " + num2 + " = " + (+num1 + +num2));}
+else if (op == "-") {alert(num1 + " - " + num2 + " = " + (+num1 - +num2));}
+else if (op == "*") {alert(num1 + " * " + num2 + " = " + (+num1 * +num2));}
+else if (op == "/") {alert(num1 + " / " + num2 + " = " + (+num1 / +num2));}
+else if (op == "%") {alert(num1 + "% of " + num2 " = " + (+num1 * +num2 / 100));}
+else if (op == "^") {alert(num1 + "^" + num2 " = " + Math.pow(+num1 * +num2);}
+else if (op == "R") {alert(num1 + "/" + num2 + " = " Math.floor(+num1 / +num2) + "R" + (+num1 % +num2));}
 else {alert("operation error");}
+}
